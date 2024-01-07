Fix a bug in radii calculation

Models/hmlp_ibp.py
@@ -10,6 +10,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from copy import deepcopy
+
 class HMLP_IBP(HMLP, HyperNetInterface):
 
     """
@@ -26,7 +28,7 @@ class HMLP_IBP(HMLP, HyperNetInterface):
     def __init__(self, 
                 target_shapes, 
                 cond_in_size, 
-                num_cond_embs=1,  
+                num_cond_embs=1,
                 *args, 
                 **kwargs): 
         
@@ -38,19 +40,12 @@ class HMLP_IBP(HMLP, HyperNetInterface):
         self.trained_radii = torch.zeros(cond_in_size)    # Initialize an empty tensor for intervals
                                                           # around embeddings in the weight space
         self.tasks_embeddings = torch.zeros(cond_in_size) # This variable stores the learned embedding
-
-    @property
-    def internal_params(self):
-        return self._internal_params
-    
-    @internal_params.setter
-    def internal_params(self, cond_id, value):
-        self._internal_params[cond_id] = value
+        self.perturbated_eps = kwargs["perturbated_eps"]
 
     def forward(self, uncond_input=None, cond_input=None, cond_id=None,
                 weights=None, distilled_params=None, condition=None,
                 ret_format='squeezed', return_extended_output = False,
-                perturbated_eps = 0.5):
+                perturbated_eps = None):
         """Compute the weights of a target network.
 
         Args:
@@ -73,6 +68,8 @@ class HMLP_IBP(HMLP, HyperNetInterface):
                 cond_input=cond_input, cond_id=cond_id, weights=weights,
                 distilled_params=distilled_params, condition=condition,
                 ret_format=ret_format)
+        
+        perturbated_eps = self.perturbated_eps if perturbated_eps is None else perturbated_eps
 
         ### Prepare hypernet input ###
         assert self._uncond_in_size == 0 or uncond_input is not None
@@ -115,27 +112,46 @@ class HMLP_IBP(HMLP, HyperNetInterface):
         if self._use_batch_norm:
             assert len(bn_scales) == len(fc_weights) - 1
 
-        ### Process inputs through network ###
+        ### Process inputs through the network ###
             
-        
         # Normalization step - we give to the neural net a chance to
         # decide about length of interval around each dimension of
         # embedding
-        eps = perturbated_eps*F.softmax(torch.ones_like(h), dim=1)
+        eps = perturbated_eps*F.softmax(h, dim=1)
 
         # Store the trained radii
-        self.trained_radii = eps 
+        self.trained_radii = deepcopy(eps.detach())
 
-        for i in range(len(fc_weights)):            
-            
-            h   = F.linear(h, fc_weights[i], bias=fc_biases[i])
-            eps = F.linear(eps, torch.abs(fc_weights[i]), 
-                           bias=torch.zeros_like(fc_biases[i]))
-            z_l, z_u = h - eps, h + eps
-            z_l, z_u = F.relu(z_l), F.relu(z_u)
-            h, eps = (z_u + z_l)/2, (z_u - z_l)/2
+        for i in range(len(fc_weights)):
+            last_layer = i == (len(fc_weights) - 1)
 
-        self.tasks_embeddings = h   # Store the embedding
+            h = F.linear(h, fc_weights[i], bias=fc_biases[i])
+
+            # Calculate adaptive weights
+            W   = torch.abs(fc_weights[i])
+            eps = F.linear(eps, W, bias=torch.zeros_like(fc_biases[i]))
+
+            if not last_layer:
+                # Batch-norm
+                if self._use_batch_norm:
+                    h = self.batchnorm_layers[i].forward(h, running_mean=None,
+                        running_var=None, weight=bn_scales[i],
+                        bias=bn_shifts[i], stats_id=condition)
+                
+                # Dropout
+                if self._dropout_rate != -1:
+                    h = self._dropout(h)
+
+                # Non-linearity
+                if self._act_fn is not None:
+                    z_l, z_u = h - eps, h + eps
+                    z_l, z_u = self._act_fn(z_l), self._act_fn(z_u)
+                    h, eps   = (z_u + z_l) / 2, (z_u - z_l) / 2
+
+        z_l, z_u = h-eps, h+eps
+        
+        # Store the embedding
+        self.tasks_embeddings = deepcopy(h.detach())
 
         ### Split output into target shapes ###
         ret = self._flat_to_ret_format(h, ret_format)