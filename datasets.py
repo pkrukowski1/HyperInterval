7d2bc47998bb534d0d4b507e305eaffac350625b
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2024-03-17 22:33:45 +0100

    Change parameters

--- datasets.py
+++ datasets.py
@@ -231,7 +231,7 @@ def set_hyperparameters(dataset,
                 'perturbated_epsilon': [10.0],
                 'hypernetworks_hidden_layers': [[100, 100]],
                 'dropout_rate': [-1],
-                'best_model_selection_method': 'last_model',
+                'best_model_selection_method': 'val_loss',
                 'saving_folder': './Results/'
                 f'permuted_mnist_final_grid_experiments/last_model/'
             }
