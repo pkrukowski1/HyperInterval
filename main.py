147b617e11deff9b73378ba95700cbb66ebfb3c8
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2024-03-18 19:07:55 +0100

    Fix bug in calculation of intervals' intersection

--- main.py
+++ main.py
@@ -97,11 +97,14 @@ def intersection_of_embeds(z_l: torch.Tensor, z_u: torch.Tensor) -> Tuple[torch.
 
     z_l_common_embed = z_l.max(dim=0).values
     z_u_common_embed = z_u.min(dim=0).values
-    z_u_common_embed  = torch.where(z_u_common_embed < z_l_common_embed, (z_u_common_embed + z_l_common_embed)/2, z_u_common_embed)
-    z_l_common_embed  = torch.where(z_u_common_embed < z_l_common_embed, (z_u_common_embed + z_l_common_embed)/2, z_l_common_embed)
 
-    list_of_violations = [idx for idx, el in enumerate(z_u_common_embed - z_l_common_embed < 0) if el]
-    assert list_of_violations == [], f"These dimensions of the task embeddings have an empty intersection: {list_of_violations}"
+    z_u_common_embed_final = torch.where(z_u_common_embed < z_l_common_embed, (z_u_common_embed + z_l_common_embed)/2, z_u_common_embed)
+    z_l_common_embed_final = torch.where(z_u_common_embed < z_l_common_embed, (z_u_common_embed + z_l_common_embed)/2, z_l_common_embed)
+
+    assert (z_l_common_embed_final <= z_u_common_embed_final).all(), f"Upper bounds should be greater or equal to lower bounds, upper: {z_u_common_embed_final}, lower: {z_l_common_embed_final}"
+
+    # list_of_violations = [idx for idx, el in enumerate(z_u_common_embed - z_l_common_embed < 0) if el]
+    # assert list_of_violations == [], f"These dimensions of the task embeddings have an empty intersection: {list_of_violations}"
 
     return z_l_common_embed, z_u_common_embed
 
